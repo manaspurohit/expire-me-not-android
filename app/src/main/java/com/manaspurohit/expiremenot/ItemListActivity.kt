0d92a523ceba7a9751c3630ef251bf3493847d53
Manas Purohit <purohit.m@husky.neu.edu>
2017-09-21 19:47:02 -0700

    added floating action button and recycler view

--- app/src/main/java/com/manaspurohit/expiremenot/ItemListActivity.kt
+++ app/src/main/java/com/manaspurohit/expiremenot/ItemListActivity.kt
@@ -3,11 +3,10 @@ package com.manaspurohit.expiremenot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class FoodListActivity : AppCompatActivity() {
+class ItemListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_list)
-
+        setContentView(R.layout.activity_item_list)
     }
 }
