106599bc699812decbc5fc6ea72e3a400428f598
Manas Purohit <purohit.m@husky.neu.edu>
2017-09-25 20:14:12 -0700

    refactored adding / editing items

--- app/src/main/java/com/manaspurohit/expiremenot/ItemListActivity.kt
+++ app/src/main/java/com/manaspurohit/expiremenot/ItemListActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.helper.ItemTouchHelper
 import android.text.TextUtils
 import android.widget.EditText
 import com.manaspurohit.expiremenot.adapter.ItemRecyclerAdapter
+import com.manaspurohit.expiremenot.data.Item
 import com.manaspurohit.expiremenot.touch.ItemTouchHelperCallback
 import kotlinx.android.synthetic.main.activity_item_list.*
 import kotlinx.android.synthetic.main.dialog_add_item.*
@@ -40,64 +41,32 @@ class ItemListActivity : AppCompatActivity() {
     }
 
     private fun showAddItemDialog() {
-        val dialogBuilder = AlertDialog.Builder(this)
-        dialogBuilder.setTitle(getString(R.string.add_perish_item))
-
-        dialogBuilder.setView(R.layout.dialog_add_item)
-
-        dialogBuilder.setPositiveButton(getString(R.string.ok)) { _, _ -> }
-
-        dialogBuilder.setNeutralButton(getString(R.string.take_a_pic)) { _, _ ->
-            // TODO: Change to camera view
-        }
-
-        dialogBuilder.setNegativeButton(getString(R.string.cancel)) { _, _ -> }
-
-        val dialog = dialogBuilder.create()
-        dialog.show()
+        val dialog = showDialog(getString(R.string.add_perish_item))
 
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            val etName = dialog.findViewById(R.id.etName) as? EditText
-            val etDate = dialog.findViewById(R.id.etDate) as? EditText
-
-            if (etName == null || etDate == null) {
-                return@setOnClickListener
+            val item = inputToItem(dialog)
+            item?.let {
+                itemRecyclerAdapter.addItem(it.name, it.expireDate)
+                dialog.dismiss()
             }
-
-            if (TextUtils.isEmpty(etName.text)) {
-                etName.error = getString(R.string.name_empty_error)
-                return@setOnClickListener
-            }
-
-            val name: String = etName.text.toString()
-
-            val sdf = SimpleDateFormat(getString(R.string.date_format), Locale.US)
-            val expireDate: Date = try {
-                val initialDate = sdf.parse(etDate.text.toString())
-                val cal = Calendar.getInstance()
-                cal.time = initialDate
-                cal.add(Calendar.HOUR_OF_DAY, 23)
-                cal.time
-            }
-            catch (e: ParseException) {
-                etDate.error = "Please use mm/dd/yyyy format"
-                return@setOnClickListener
-            }
-
-            if (Calendar.getInstance().time.after(expireDate)) {
-                etDate.error = "Please enter a valid date"
-                return@setOnClickListener
-            }
-
-            itemRecyclerAdapter.addItem(name, expireDate)
-
-            dialog.dismiss()
         }
     }
 
     fun showEditItemDialog(curName: String, curDate: String, position: Int) {
+        val dialog = showDialog(getString(R.string.edit_perish_item), curName, curDate)
+
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val item = inputToItem(dialog)
+            item?.let {
+                itemRecyclerAdapter.editItem(it.name, it.expireDate, position)
+                dialog.dismiss()
+            }
+        }
+    }
+
+    private fun showDialog(title: String, nameText: String = "", dateText: String = ""): AlertDialog {
         val dialogBuilder = AlertDialog.Builder(this)
-        dialogBuilder.setTitle("Edit perishable item")
+        dialogBuilder.setTitle(title)
 
         dialogBuilder.setView(R.layout.dialog_add_item)
 
@@ -112,46 +81,46 @@ class ItemListActivity : AppCompatActivity() {
         val dialog = dialogBuilder.create()
         dialog.show()
 
-        dialog.etName.setText(curName)
-        dialog.etDate.setText(curDate)
+        dialog.etName.setText(nameText)
+        dialog.etDate.setText(dateText)
 
-        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-            val etName = dialog.findViewById(R.id.etName) as? EditText
-            val etDate = dialog.findViewById(R.id.etDate) as? EditText
+        return dialog
+    }
 
-            if (etName == null || etDate == null) {
-                return@setOnClickListener
-            }
+    private fun inputToItem(dialog: AlertDialog): Item? {
+        val etName = dialog.findViewById(R.id.etName) as? EditText
+        val etDate = dialog.findViewById(R.id.etDate) as? EditText
 
-            if (TextUtils.isEmpty(etName.text)) {
-                etName.error = getString(R.string.name_empty_error)
-                return@setOnClickListener
-            }
-
-            val name: String = etName.text.toString()
-
-            val sdf = SimpleDateFormat(getString(R.string.date_format), Locale.US)
-            val expireDate: Date = try {
-                val initialDate = sdf.parse(etDate.text.toString())
-                val cal = Calendar.getInstance()
-                cal.time = initialDate
-                cal.add(Calendar.HOUR_OF_DAY, 23)
-                cal.time
-            }
-            catch (e: ParseException) {
-                etDate.error = "Please use mm/dd/yyyy format"
-                return@setOnClickListener
-            }
-
-            if (Calendar.getInstance().time.after(expireDate)) {
-                etDate.error = "Please enter a valid date"
-                return@setOnClickListener
-            }
-
-            itemRecyclerAdapter.editItem(name, expireDate, position)
-
-            dialog.dismiss()
+        if (etName == null || etDate == null) {
+            return null
         }
+
+        if (TextUtils.isEmpty(etName.text)) {
+            etName.error = getString(R.string.name_empty_error)
+            return null
+        }
+
+        val name: String = etName.text.toString()
+
+        val sdf = SimpleDateFormat(getString(R.string.date_format), Locale.US)
+        val expireDate: Date = try {
+            val initialDate = sdf.parse(etDate.text.toString())
+            val cal = Calendar.getInstance()
+            cal.time = initialDate
+            cal.add(Calendar.HOUR_OF_DAY, 23)
+            cal.time
+        }
+        catch (e: ParseException) {
+            etDate.error = "Please use mm/dd/yyyy format"
+            return null
+        }
+
+        if (Calendar.getInstance().time.after(expireDate)) {
+            etDate.error = "Please enter a valid date"
+            return null
+        }
+
+        return Item(name, expireDate)
     }
 
     override fun onDestroy() {
